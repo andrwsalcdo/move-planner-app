5725068b979e8ed8c9dfe27f2eee53bb49a9f17d
AndrewSalcedo <asalcedo07@gmail.com>
2017-02-05 13:58:13 -0500

    fix: loadData(); The data didn't load onto the html. add: .fail(function(e)); to NYT api

--- js/script.js
+++ js/script.js
@@ -24,18 +24,23 @@ function loadData() {
       NYTIMES
       API Key for the Article Search API: 7d61a6720f1c464dbb2473ebb4823011
     */
-    var nytimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr +'&sort=newest&api-key=7d61a6720f1c464dbb2473ebb4823011'
+    var nytimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q='+cityStr+'&sort=newest&api-key=7d61a6720f1c464dbb2473ebb4823011';
 
     $.getJSON(nytimesURL, function(data) {
 
       $nytHeaderElem.text('New York Times Articles about ' + cityStr);
 
       articles = data.response.docs;
-      for (i=0; i<articles.length; i++) {
+      for (var i=0; i < articles.length; i++) {
         var article = articles[i];
-        $nytElem.append('<li class="article">' + '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+'<p>' + article.snippet + '</p>'+ '</li>');
+        $nytElem.append('<li class="article">' +
+          '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+          '<p>' + article.snippet + '</p>'+
+        '</li>');
       };
-    })
+    }).fail(function(e) {
+      $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
 
     $.getJSON();
 
@@ -47,3 +52,5 @@ function loadData() {
 
     return false;
 };
+
+$('#form-container').submit(loadData);
